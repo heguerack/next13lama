2ee3dc6a21146864e3b6704705429f9c9a68bb41
Frank <heguer76@gmail.com>
2023-07-08 20:19:55 -0600

    dev env domain

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 async function getData() {
   // const res = await fetch('http://localhost:3000/api/posts/', {
-  const res = await fetch('http://localhost:3000/api/posts/', {
+  const res = await fetch('https://next13lama.vercel.app/api/posts/', {
     // { next: { revalidate: 10 } }
     cache: 'no-store',
   })
@@ -16,25 +16,24 @@ async function getData() {
 }
 
 export default async function BlogPage() {
-  return <h1>Im blog</h1>
-  // const data = await getData()
-  // console.log(data)
-  // return (
-  //   <div className={styles.mainContainer}>
-  //     {data?.map((blog) => {
-  //       const { userId, _id: id, title, content, image } = blog
-  //       return (
-  //         <Link href={`/blog/${id}`} className={styles.container} key={id}>
-  //           <div className={styles.imgContainer}>
-  //             <Image src={image} fill={true} className={styles.img} />
-  //           </div>
-  //           <div className={styles.content}>
-  //             <h1 className={styles.title}>{title}</h1>
-  //             <p className={styles.desc}>{content}</p>
-  //           </div>
-  //         </Link>
-  //       )
-  //     })}
-  //   </div>
-  // )
+  const data = await getData()
+  console.log(data)
+  return (
+    <div className={styles.mainContainer}>
+      {data?.map((blog) => {
+        const { userId, _id: id, title, content, image } = blog
+        return (
+          <Link href={`/blog/${id}`} className={styles.container} key={id}>
+            <div className={styles.imgContainer}>
+              <Image src={image} fill={true} className={styles.img} />
+            </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{title}</h1>
+              <p className={styles.desc}>{content}</p>
+            </div>
+          </Link>
+        )
+      })}
+    </div>
+  )
 }
